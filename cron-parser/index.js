9df57657930b1a2651f2f503f88677b75281a254
Tyler Reece <tyler.reece@imply.io>
2021-06-24 09:45:58 -0400

    Added UTC test to cron parser

--- cron-parser/index.js
+++ cron-parser/index.js
@@ -4,9 +4,9 @@ console.log('---------- Testing every 30 minutes ----------')
 try {
     const interval = parser.parseExpression('* */30 * * * *');
 
-    console.log('Next Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+    console.log('Next Date: ', interval.next().toString());
 
-    console.log('Prev Date: ', interval.prev().toString()); // Sat Dec 29 2012 00:40:00 GMT+0200 (EET)
+    console.log('Prev Date: ', interval.prev().toString());
 } catch (err) {
     console.log('Error: ' + err.message);
 }
@@ -15,9 +15,20 @@ console.log('---------- Testing every 0th minute ----------')
 try {
     const interval = parser.parseExpression('* 0 * * * *');
 
-    console.log('Next Date: ', interval.next().toString()); // Sat Dec 29 2012 00:42:00 GMT+0200 (EET)
+    console.log('Next Date: ', interval.next().toString());
 
-    console.log('Prev Date: ', interval.prev().toString()); // Sat Dec 29 2012 00:40:00 GMT+0200 (EET)
+    console.log('Prev Date: ', interval.prev().toString());
+} catch (err) {
+    console.log('Error: ' + err.message);
+}
+
+console.log('---------- Testing every 0th minute UTC ----------')
+try {
+    const interval = parser.parseExpression('* 0 * * * *', {tz: 'Etc/UTC', currentDate: new Date('Thu Jun 24 2021 09:20:21 GMT-0400')});
+
+    console.log('Next Date: ', interval.next().toDate());
+
+    console.log('Prev Date: ', interval.prev().toDate());
 } catch (err) {
     console.log('Error: ' + err.message);
 }
